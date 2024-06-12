46397d7f7551d8c11ac33e9f1ec9f07ab4a484aa
hix <canbedir100@gmail.com>
2024-06-12 20:44:06 +0300

    Server settings

--- src/components/server/server-header.tsx
+++ src/components/server/server-header.tsx
@@ -52,7 +52,8 @@ const ServerHeader = ({ server, role }: ServerHeaderProps) => {
           </DropdownMenuItem>
         )}
         {isAdmin && (
-          <DropdownMenuItem 
+          <DropdownMenuItem
+           onClick={()=>onOpen("editServer",{server})} 
            className="px-3 py-2 text-sm cursor-pointer text-black focus:text-white dark:text-white/75 focus:bg-indigo-500 dark:hover:text-white">
             Server Settings
             <Settings className="h-4 w-4 ml-auto" />
