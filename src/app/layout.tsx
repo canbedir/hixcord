cfdb01b3fab4a15529436c2619ba6f139891f5e2
hix <canbedir100@gmail.com>
2024-06-17 01:32:58 +0300

    Socket IO setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { cn } from "@/lib/utils";
 import { ModalProvider } from "@/components/providers/modal-provider";
+import { SocketProvider } from "@/components/providers/socket-provider";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -21,10 +22,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={cn(
-          font.className,
-          "bg-white dark:bg-[#313338]"
-          )}>
+        <body className={cn(font.className, "bg-white dark:bg-[#313338]")}>
           <ThemeProvider
             attribute="class"
             defaultTheme="dark"
@@ -32,8 +30,10 @@ export default function RootLayout({
             storageKey="hixcord-theme"
             disableTransitionOnChange
           >
-            <ModalProvider/>
-            {children}
+            <SocketProvider>
+              <ModalProvider />
+              {children}
+            </SocketProvider>
           </ThemeProvider>
         </body>
       </html>
