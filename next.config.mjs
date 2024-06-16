2ae9b4219850a4e64e8dc0788f18b3a2fcb20791
hix <canbedir100@gmail.com>
2024-06-16 22:20:56 +0300

    chore: update image domains configuration to use remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        domains:[
-            "uploadthing.com",
-            "utfs.io"
-        ]
-    }
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'uploadthing.com',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'utfs.io',
+                port: '',
+                pathname: '/**',
+            },
+        ],
+    },
 };
 
 export default nextConfig;
